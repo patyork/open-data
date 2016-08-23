29e702030c4dbd486d2eb9108eee1bb0e7bec621
Pat <pat.york@nevada.unr.edu>
2016-08-23 15:22:55 -0700

    More error handling; correct HTTP status codes on error

--- flaskApi.py
+++ flaskApi.py
@@ -16,14 +16,22 @@ class Status(Resource):
 class AssessorSearch(Resource):
 	def get(self):
 		parser = reqparse.RequestParser(bundle_errors=True)
-		parser.add_argument('parcelNum', required=True, type=unicode, help="Parcel Number")
+		parser.add_argument('parcelNum', type=unicode, help="Parcel Number starting value")
+		parser.add_argument('parcelNumRange', type=unicode, help="Parcel Number value range end")
+		parser.add_argument('ownerName', type=unicode, help="Partial Owner Name")
+		parser.add_argument('location', type=unicode, help="Parial Location")
 		
 		args = parser.parse_args()
+		
+		if sum([1 for x in args.values() if x is not None]) == 0:
+			abort(400, message='At least one search term is required.')
+		elif args['parcelNumRange'] is not None and (args['parcelNum'] is None  or len(args['parcelNum']) == 0):
+			abort(400, message='parcelNum must be included in a ranged search.')
 		
 		try:
 			return {'results' : H.search({'srchpar1' : args['parcelNum']})}
 		except r.ConnectionError as e:
-			abort(503, 'Underlying connection to the governmental data source is unavaiable (County or City website down!)')
+			abort(504, message='Underlying connection to the governmental data source is unavaiable (County or City website down!)')
 
 api.add_resource(Status, '/')
 api.add_resource(AssessorSearch, '/search')
